64784c1fc64e98de8c02efbff1d8ab2be5019c78
trungitk16nbk <buiquoctrung2002@gmail.com>
2023-09-19 14:19:39 +0700

    add cros

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const express = require("express");
 const app = express();
 const fs = require("fs");
 const PORT = process.env.PORT || 5000;
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'chrome-extension://khjnjdmnalkmfgakpkpponbkadajalnn');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 const productsData = fs.readFileSync("products.txt", "utf-8");
 const products = JSON.parse(productsData);
